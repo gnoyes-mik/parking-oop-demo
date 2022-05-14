7a14e1e7b45f97e08a16969e8534313ed9fed442
gnoyes <sedragon.kim@gmail.com>
2022-05-14 22:20:38 +0900

    feat: enter, park, moveOut 기능 추가 및 개선

--- src/main/java/domain/parkingLot/StandardParkingLot.java
+++ src/main/java/domain/parkingLot/StandardParkingLot.java
@@ -3,20 +3,47 @@ package domain.parkingLot;
 import domain.car.Car;
 import domain.parkingBox.BusParkingBox;
 import domain.parkingBox.GeneralCarParkingBox;
+import domain.parkingBox.LightCarParingBox;
 import domain.parkingBox.ParkingBox;
+import domain.parkingLot.dto.CarParkingInfo;
+import domain.parkingLot.dto.ParkingReceipt;
+import domain.parkingLot.policy.ParkingCostPolicy;
+import lombok.extern.slf4j.Slf4j;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Slf4j
 public class StandardParkingLot implements ParkingLot {
 
     private ParkingBox[][] parkingLot;
+    private ParkingCostPolicy costPolicy;
     private final int MAX_FLOOR;
     private final int MAX_NO;
     private final int MIN_FLOOR = 1;
     private final int MIN_NO = 1;
+    private final String PARKING_LOT_NAME = "STANDARD_PARK";
 
-    public StandardParkingLot(int floor, int no) {
+    public StandardParkingLot(int floor, int no, ParkingCostPolicy costPolicy) {
+        validateConstructInfo(floor, no, costPolicy);
+
         this.parkingLot = generateParkingBoxes(floor, no);
         this.MAX_FLOOR = floor;
         this.MAX_NO = no;
+        this.costPolicy = costPolicy;
+    }
+
+    private void validateConstructInfo(int floor, int no, ParkingCostPolicy costPolicy) {
+        if (floor <= 0) {
+            throw new IllegalArgumentException("floor 값은 0 이하가 될 수 없습니다");
+        }
+        if (no <= 0) {
+            throw new IllegalArgumentException("no 값은 0 이하가 될 수 없습니다");
+        }
+        if (Objects.isNull(costPolicy)) {
+            throw new IllegalArgumentException("주차 비용 정책이 없습니다");
+        }
     }
 
     private ParkingBox[][] generateParkingBoxes(int floor, int no) {
@@ -27,38 +54,104 @@ public class StandardParkingLot implements ParkingLot {
                     parkingLot[f][n] = new BusParkingBox();
                     continue;
                 }
-                parkingLot[f][n] = new GeneralCarParkingBox();
+                if (n % 2 == 0) {
+                    parkingLot[f][n] = new GeneralCarParkingBox();
+                    continue;
+                }
+                parkingLot[f][n] = new LightCarParingBox();
             }
         }
         return parkingLot;
     }
 
     @Override
-    public void park(int floor, int no, Car car) {
-        validateParam(floor, no);
-
-        if (parkingLot[floor][no].status()) {
-            throw new RuntimeException("차량이 이미 존재합니다");
-        }
-
-        parkingLot[floor][no].in(car);
-        System.out.println("차량 번호 [" + car.getCarNum() + "]가 주차되었습니다");
+    public CarParkingInfo enter(Car car) {
+        validateParam(car);
+        CarParkingInfo carParkingInfo = new CarParkingInfo(PARKING_LOT_NAME, car);
+        carParkingInfo.recordEnterTime();
+        return carParkingInfo;
     }
 
     @Override
-    public void moveOut(int floor, int no, Car car) {
-        parkingLot[floor][no].out();
-        System.out.println("차량 번호 [" + car.getCarNum() + "]가 출차되었습니다");
+    public CarParkingInfo leave(Car car) {
+        validateParam(car);
+
+        return new CarParkingInfo();
     }
 
-    private void validateParam(int floor, int no) {
+    @Override
+    public void park(CarParkingInfo info) {
+        validateParam(info);
+
+        int floor = info.getFloor();
+        int no = info.getParkingBoxNo();
+
+        if (parkingLot[floor][no].status()) {
+            log.error("{}층 {}번에 차량이 이미 존재합니다", info.getFloor(), info.getParkingBoxNo());
+            throw new RuntimeException("차량이 이미 존재합니다");
+        }
+
+        Car car = info.getCar();
+        parkingLot[floor][no].in(car);
+
+        log.info("차량 번호 [{}]가 {}층 {}번에 주차되었습니다", car.getCarNum(), info.getFloor(), info.getParkingBoxNo());
+    }
+
+    @Override
+    public void moveOut(CarParkingInfo info) {
+        validateParam(info);
+
+        int floor = info.getFloor();
+        int no = info.getParkingBoxNo();
+
+        Car car = info.getCar();
+        if (!parkingLot[floor][no].status()) {
+            log.error("{}층 {}번에 이미 차량이 존재하지 않습니다", info.getFloor(), info.getParkingBoxNo());
+            throw new RuntimeException("주차 칸에 차량이 존재하지 않습니다");
+        }
+
+        parkingLot[floor][no].out();
+        log.info("차량 번호 [{}]가 출차되었습니다", car.getCarNum());
+    }
+
+    private void validateParam(Car car) {
+        if (Objects.isNull(car)) {
+            throw new IllegalArgumentException("자동차 정보는 필수입니다");
+        }
+        if (Objects.isNull(car.getCarNum()) || car.getCarNum().isEmpty()) {
+            throw new IllegalArgumentException("차량 번호를 다시 확인해주세요");
+        }
+    }
+
+    private void validateParam(CarParkingInfo info) {
+        if (Objects.isNull(info) || Objects.isNull(info.getLocation())) {
+            log.error("location 정보는 필수 입니다");
+            throw new RuntimeException("location 정보는 필수 입니다");
+        }
+
+        int floor = info.getFloor();
+        int no = info.getParkingBoxNo();
         if (floor < MIN_FLOOR || no < MIN_NO) {
-            throw new RuntimeException("최솟값 작습니다");
+            log.error("floor 또는 no 값이 비정상적으로 작습니다");
+            throw new RuntimeException("최솟값보다 작습니다");
         }
 
         if (floor > MAX_FLOOR || no > MAX_NO) {
-            throw new RuntimeException("최댓값 큽니다");
+            log.error("floor 또는 no 값이 비정상적으로 큽니다");
+            throw new RuntimeException("최댓값보다 큽니다");
         }
+    }
+
+    @Override
+    public ParkingReceipt calculate(CarParkingInfo info) {
+        LocalDateTime enterTime = info.getEnterTime();
+        LocalDateTime leaveTime = info.getLeaveTime();
+        Duration duration = Duration.between(enterTime, leaveTime);
+        long min = duration.toMinutes();
+        long cost = min * costPolicy.getCostPer10min();
+
+        log.info("[{}] 기간 : {} ~ {} / 비용 : {}(원)", info.getCar().getCarNum(), enterTime, leaveTime, cost);
+        return new ParkingReceipt(cost);
     }
 
     @Override
