fc70193feead80084f144d370e62287f62af7b5b
gnoyes <sedragon.kim@gmail.com>
2022-05-14 22:23:53 +0900

    delete

--- src/main/java/domain/parkingLot/아리스타주차장.java
+++ src/main/java/domain/parkingLot/아리스타주차장.java
@@ -1,20 +1,32 @@
 package domain.parkingLot;
 
 import domain.car.Car;
+import domain.parkingLot.dto.CarParkingInfo;
 
-public class 아리스타주차장 implements ParkingLot {
-    @Override
-    public void park(int floor, int no, Car car) {
-
-    }
-
-    @Override
-    public void moveOut(int floor, int no, Car car) {
-
-    }
-
-    @Override
-    public void show() {
-
-    }
-}
+//public class 아리스타주차장 implements ParkingLot {
+//
+//    @Override
+//    public CarParkingInfo enter(Car car) {
+//        return null;
+//    }
+//
+//    @Override
+//    public CarParkingInfo leave(Car car) {
+//        return null;
+//    }
+//
+//    @Override
+//    public void park(CarParkingInfo info) {
+//
+//    }
+//
+//    @Override
+//    public void moveOut(CarParkingInfo info) {
+//
+//    }
+//
+//    @Override
+//    public void show() {
+//
+//    }
+//}
