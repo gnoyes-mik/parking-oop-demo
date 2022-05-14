b5e4b9036ccb641a058c1cdb04e4967ea23fbd75
gnoyes <sedragon.kim@gmail.com>
2022-05-14 22:19:55 +0900

    feat: Location 유틸클래스 생성

---
+++ src/main/java/utils/Location.java
@@ -0,0 +1,30 @@
+package utils;
+
+public class Location {
+    private int floor;
+    private int no;
+
+    public Location() {
+    }
+
+    public Location(int floor, int no) {
+        this.floor = floor;
+        this.no = no;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+}
